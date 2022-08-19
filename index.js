b344943e874f56e3aa694e816182c7b6d663eb2d
samuelnelson176 <106776244+samuelnelson176@users.noreply.github.com>
2022-08-19 10:41:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const date = new Date();
+
+const month = date.getMonth();
+
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "Deccember"
+];
+
+document.querySelector(".date h1").innerHTML = months[date.getMonth()]; 
+
+document.querySelector(".date p").innerHTML = date.toDateString();
+
+const monthDays = document.querySelector('.days')
+
+let days = "";
+
+for(let i=1; i<=31; i++){
+    days += `<div>${i}</div>`;
+    monthDays.innerHTML = days;
+}
