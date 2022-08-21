a48519606ce6569160897cfab782bc43bb8d09be
samuelnelson176 <106776244+samuelnelson176@users.noreply.github.com>
2022-08-22 00:34:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const date = new Date();
 
-const month = date.getMonth();
+const monthDays = document.querySelector('.days')
 
+const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+const firstDayIndex = date.getDay();
+
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay();
 
 const months = [
     "January",
@@ -22,14 +28,13 @@ document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 
 document.querySelector(".date p").innerHTML = date.toDateString();
 
-const monthDays = document.querySelector('.days')
-
 let days = "";
+
+for(let x = firstDayIndex; x > 0; x--){
+    days += `<div class = "prevDate">${prevLastDay - x}</div>`
+};
 
 for(let i=1; i<=31; i++){
     days += `<div>${i}</div>`;
     monthDays.innerHTML = days;
 };
-
-const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0);
-
