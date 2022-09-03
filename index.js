486d44f4f895227cc6bd3197cd14de2f0aee1ce7
samuelnelson176 <106776244+samuelnelson176@users.noreply.github.com>
2022-09-03 13:50:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+//creating the date object
 const date = new Date();
 
-const monthDays = document.querySelector('.days')
+date.setDate(1);
 
-const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+console.log(date.getDay());
 
-const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+//declearing the first day variable
 const firstDayIndex = date.getDay();
 
-const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay();
+//creating the month object
+const month = date.getMonth();
 
+//updates the displayed days using the loop created
+const monthDays = document.querySelector(".days");
+
+//dynamically updating the last day for each month
+const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+
+//
+const prevlastDay =  new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+const lastDayIndex =  new Date(date.getFullYear(), date.getMonth()+1, 0).getDay();
+
+const nextDays = 7-lastDayIndex-1;
+
+console.log(nextDays)
+
+//Creating the months array
 const months = [
     "January",
     "February",
@@ -21,20 +39,31 @@ const months = [
     "September",
     "October",
     "November",
-    "Deccember"
+    "December"
 ];
 
-document.querySelector(".date h1").innerHTML = months[date.getMonth()]; 
+//updating the displayed month to present month
+document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 
+//updating the displayed date to current date
 document.querySelector(".date p").innerHTML = date.toDateString();
 
+//days varaible
 let days = "";
 
-for(let x = firstDayIndex; x > 0; x--){
-    days += `<div class = "prevDate">${prevLastDay - x}</div>`
+//loop for the last days of previous month index
+for(let x = firstDayIndex; x>0; x--){
+   days += `<div class = "prevDate">${prevlastDay-x+1}</div>`;
 };
 
-for(let i=1; i<=31; i++){
-    days += `<div>${i}</div>`;
+//loop for the first days of next month index
+for(let j = 1; j <= nextDays; j++){
+    days += `<div class = "nextDate">${j}</div>`;
+ };
+ 
+
+//loop for the days
+for (let index = 1; index <= lastDay; index++) {
+    days += `<div>${index}</div>`;
     monthDays.innerHTML = days;
 };
